343c5ab41c8c60067ee15dc609f1bcbeffc7634d
Marzuki711 <113434411+Marzuki711@users.noreply.github.com>
2026-08-04 11:10:43 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,26 @@
+/*************************************************
+ * Manual OT Claim System
+ * config.js
+ * Version : V2.0 (GitHub Pages)
+ *************************************************/
 
+const CONFIG = {
+
+    // ==========================================
+    // Google Apps Script Web App URL
+    // ==========================================
+
+    WEB_APP_URL:
+    "https://script.google.com/macros/s/AKfycbxU7UJJZZm-VAX7jgkba9d0QfZ93hvJs_Z9OIcIeo0H1Aza7OQUacHjo1L_RzQc9DIj4g/exec",
+
+    // ==========================================
+    // Application
+    // ==========================================
+
+    APP_NAME: "Manual OT Claim",
+
+    VERSION: "2.0",
+
+    DEBUG: true
+
+};
