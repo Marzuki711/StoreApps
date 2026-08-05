a948c697103c8755672860ed8270c086cc1d5fbc
Marzuki711 <113434411+Marzuki711@users.noreply.github.com>
2026-08-05 16:45:04 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,64 @@
+/*************************************************
+ * AUTHENTICATION
+ * StoreApps V1
+ *************************************************/
 
+let currentUser = null;
+
+/* ==========================================
+   LOGIN
+========================================== */
+
+async function loginSystem(){
+
+    const username =
+        document.getElementById("loginUsername")
+        .value
+        .trim();
+
+    const password =
+        document.getElementById("loginPassword")
+        .value;
+
+    if(username === ""){
+
+        showError("Please enter Username.");
+
+        return;
+
+    }
+
+    if(password === ""){
+
+        showError("Please enter Password.");
+
+        return;
+
+    }
+
+    const result = await callAPI("login",{
+
+        username:username,
+
+        password:password
+
+    });
+
+    if(!result.status){
+
+        showError(result.message);
+
+        return;
+
+    }
+
+    currentUser = result;
+
+    // Hide Login
+    document.getElementById("loginContainer")
+        .style.display="none";
+
+    // Show Home
+    showHome();
+
+}
